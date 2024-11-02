d08553ce2a6c47f9a8cd4ee639da5309a07a1a5e
yeonupark <idepix@naver.com>
2024-11-02 20:27:12 +0900

    apply counter program

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { useSelector } from "react-redux"; 
+import { useSelector, useDispatch } from "react-redux"; 
 
 const App = () => {
-  const counterStore = useSelector((state) => state); 
+  const dispatch = useDispatch();
   const number = useSelector((state) => state.counter.number);
-  console.log(number); 
 
-  return <div></div>;
+  return <div>
+    {number}
+    <button onClick={() => {
+      // 마우스를 클릭했을 때 dispatch가 실행되고, ()안에 있는 액션객체가 리듀서로 전달됨
+      dispatch({ type: "PLUS_ONE"})
+    }}>+ 1</button>
+    <button onClick={() => {
+      dispatch({type: "MINUS_ONE"})
+    }}>- 1</button>
+  </div>;
 }
 
 export default App;
--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -5,7 +5,16 @@ const initialState = {
 
 // 리듀서: 변화를 일으키는 함수
 const counter = (state = initialState, action) => {
+    console.log(action)
     switch (action.type) {
+        case "PLUS_ONE":
+            return {
+                number: state.number + 1,
+            }
+        case "MINUS_ONE":
+            return {
+                number: state.number - 1,
+            }
         default:
             return state;
     }
